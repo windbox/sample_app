d4eacd9f303d2cb5c59261d86b95309260a6e9c7
Luke Feng <windboxx@gmail.com>
2012-04-08 21:46:27 +1000

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,16 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.1'
+
+gem 'sqlite3-ruby', :require => 'sqlite3'
+
+# Use unicorn as the web server
+# gem 'unicorn'
+
+group :development, :test do
+  gem 'rspec-rails'
+end
+
+group :test do
+  gem 'rspec-rails'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'a949c7e1bd09c6886c4e622fb9834846300e9cea90a842b54112626e9f59d0963596e8e8053b48cecd55343efe60fa7e7466303fcc80a114469a1d1eeca1c84f'
